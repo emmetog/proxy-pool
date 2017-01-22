6d18607353d2b01f35a480033a06b4931203b46b
Emmet O'Grady <emmet789@gmail.com>
2017-01-22 16:27:35 +0100

    Add new findByIpAndPort method

---
+++ src/Emmetog/ProxyPool/Exception/ProxyNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Emmetog\ProxyPool\Exception;
+
+/**
+ * Thrown when a certain proxy is requested but it doesn't exist.
+ */
+class ProxyNotFoundException extends \RuntimeException
+{
+
+}
--- src/Emmetog/ProxyPool/Repository/ProxyRepositoryInterface.php
+++ src/Emmetog/ProxyPool/Repository/ProxyRepositoryInterface.php
@@ -4,16 +4,54 @@ namespace Emmetog\ProxyPool\Repository;
 
 use Emmetog\ProxyPool\Entity\Proxy;
 use Emmetog\ProxyPool\Entity\ProxyUse;
+use Emmetog\ProxyPool\Exception\ProxyNotFoundException;
 
 interface ProxyRepositoryInterface
 {
+    /**
+     * Finds all alive proxies.
+     *
+     * @return Proxy[]
+     */
     public function findAllAlive();
 
+    /**
+     * Finds a proxy by IP and port.
+     *
+     * Throws a ProxyNotFoundException if no proxy is found.
+     *
+     * @param string $ip
+     * @param string $port
+     * @return Proxy
+     * @throws ProxyNotFoundException when the proxy doesn't exist
+     */
+    public function findByIpAndPort($ip, $port);
+
+    /**
+     * Inserts a new proxy.
+     *
+     * @param Proxy $proxy
+     */
     public function insertProxy(Proxy $proxy);
 
+    /**
+     * Records a proxy as "used" along with the result.
+     *
+     * @param ProxyUse $proxyUse
+     */
     public function insertUse(ProxyUse $proxyUse);
 
+    /**
+     * Gets the next id for a proxy.
+     *
+     * @return string
+     */
     public function getNextIdProxy();
 
+    /**
+     * Gets the next id for a proxy use.
+     *
+     * @return string
+     */
     public function getNextIdProxyUse();
 }
